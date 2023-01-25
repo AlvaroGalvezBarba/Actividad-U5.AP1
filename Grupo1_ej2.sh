f942d520ed119629137d9dc3398ec42908b9a3f7
Alvaro Galvez Barba <algalvez845@gmail.com>
2023-01-25 18:18:34 +0100

    Acabamos Grupo1_ej2.sh

--- Grupo1_ej2.sh
+++ Grupo1_ej2.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 echo "Escribe un numero: "
 read numero
-if [ $numero -gt 0 ];then
-    resto=$numero%2
-    elif [ $resto -eq 0 ]
-    echo "El número $NUMERO es par"
-    elif [ $resto -ne 0 ]
-    echo "El número $NUMERO es impar"
-else
-echo "El numero esta mal"
+let resto=$numero%2
+
+if [ $numero -lt 0 ];then
+    echo "El numero tiene que ser mayor que 0"
+elif [ $resto -eq 0 ];then
+    echo "El número $numero es par"
+elif [ $resto -ne 0 ];then
+    echo "El número $numero es impar"
 fi
