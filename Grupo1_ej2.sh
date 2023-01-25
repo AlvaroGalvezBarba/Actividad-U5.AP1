ce3a5a93cf661a23b4a2a9ffef3684ef157183eb
Alvaro Galvez Barba <algalvez845@gmail.com>
2023-01-25 18:51:02 +0100

    Modificamos Grupo1_ej2.sh y Empezamos Grupo1_ej3.sh

--- Grupo1_ej2.sh
+++ Grupo1_ej2.sh
@@ -3,8 +3,8 @@ echo "Escribe un numero: "
 read numero
 let resto=$numero%2
 
-if [ $numero -lt 0 ];then
-    echo "El numero tiene que ser mayor que 0"
+if [ $numero -le 0 ];then
+    echo "El numero tiene que ser mayor de 0"
 elif [ $resto -eq 0 ];then
     echo "El número $numero es par"
 elif [ $resto -ne 0 ];then
