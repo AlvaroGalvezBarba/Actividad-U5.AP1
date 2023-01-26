be7046050eca8a4e9e400ea24157bbd305bc0428
ALVARO GALVEZ <algaba01@floridauniversitaria.es>
2023-01-26 08:41:12 +0100

    Acabamos Grupo1_ej3.sh

--- Grupo1_ej3.sh
+++ Grupo1_ej3.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 echo "Escribe un numero: "
 read numero
-
+i=0
 if [ $numero -le 0 ];then
     echo "El numero tiene que ser mayor de 0"
-    else
-    for (( i=0; i==numero; ++i ))
-    do
-    echo $i
+else
+    until [ $i -gt $numero ];do
+    echo ${i}
+    let i=i+1
     done
 fi
